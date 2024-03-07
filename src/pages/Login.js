8ea0bf11831a5cf02e62b2a62626ddc32c081b8c
shashwanth <shashwanthsivakumarn@gmail.com>
2024-03-07 21:08:33 +0530

    create new project. initate project. created files - login register profile home. commented out the routes.

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='flex h-screen justify-center items-center w-screen'><div className='justify-center flex h-3/4 w-3/4 items-center bg-black'>login</div></div>
+  )
+}
+
+export default Login
