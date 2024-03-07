a84288df139aea140b7d334cc86287ce30fc9a96
shashwanth <shashwanthsivakumarn@gmail.com>
2024-03-07 21:22:17 +0530

    some changes to the Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
-    <div className='flex h-screen justify-center items-center w-screen'><div className='justify-center flex h-3/4 w-3/4 items-center bg-black'>login</div></div>
-  )
+    <div className="flex h-screen justify-center bg-bgPrimary items-center w-screen">
+      <div className="justify-center flex h-3/4 w-3/4 items-center bg-bgSecondary">
+        
+        login
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'bgPrimary': '#000000',
+      'bgSecondary': '#FF0000',
+      'bgAccent': '#e5741e',
+      'textPrimary': '#fce8f3',
+      'textSecondary': '#848d97',
+      'white':'#ffffff',
+      'bgTertiary':'#FE7D5D',
+      'BorderCol':'#57606a',
+      'divider': '#30363d'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
